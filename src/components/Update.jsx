af41d0588f5512ccb0c260489cdc3ddcadc4be51
Abubokkor98 <mail.abubokkor@gmail.com>
2024-11-27 22:18:44 +0600

    55-8 Explore Put And Patch And Update User To Database

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 import Navbar from './Navbar';
 
 export default function Update() {
     const loadedUser = useLoaderData();
+    const [userName, setUserName] = useState(loadedUser.name);
+
     const handleUpdate = e =>{
         e.preventDefault();
         const form = e.target;
         const name = form.name.value;
         const email = form.email.value;
-        console.log(name, email);
+        const updatedUser = {name, email};
+        console.log(updatedUser);
+
+        fetch(`http://localhost:5000/users/${loadedUser._id}`,{
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            if(data.modifiedCount>0){
+                alert('user update successfully');
+                const newName = name;
+                setUserName(newName);
+            }
+        })
     }
   return (
     <div>
         <Navbar></Navbar>
-        <h3>Update information of {loadedUser.name}</h3>
+        <h3>Update information of {userName}</h3>
         <form onSubmit={handleUpdate}>
             <input type="text" name="name" defaultValue={loadedUser?.name} id="" />
             <br />
